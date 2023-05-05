a77ed55c99fe512c88811fd47b8a09100fb04cd6
Vellmond <edgar.bautista@edu.uag.mx>
2023-05-04 17:00:50 -0700

    Initial commit

---
+++ src/main/java/com/vellmond/vellmondspring/VellmondSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vellmond.vellmondspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VellmondSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VellmondSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vellmond/vellmondspring/VellmondSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vellmond.vellmondspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VellmondSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
